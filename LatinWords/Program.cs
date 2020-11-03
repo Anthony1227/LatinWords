ca83ca697ba08c0ba214c321f4513564b084b128
anthony tran <anth.tran00@gmail.com>
2020-11-03 10:46:54 +0100

    Add project files.

---
+++ LatinWords/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatinWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command;
+            Console.WriteLine("Hello and welcome to the Latin wordlist! \nType in your word");
+
+        }
+    }
+}
